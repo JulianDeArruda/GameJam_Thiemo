83bb56b8a7ffa78cd9b31459cfdb968ecb5a3193
JulianDeArruda <julian.dearruda@web.de>
2022-09-07 17:30:50 +0200

    monster detection

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] public float detectionRange = 6;
+    [SerializeField] public GameObject player;    
+    [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private LayerMask lightLayer;
+    [SerializeField] private LayerMask barrierLayer;
+
+    private CapsuleCollider2D capsuleCollider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        search();
+    }
+
+    private void search()
+    {
+        if (shootRaycastAtPlayer(barrierLayer).collider != null){
+            Debug.Log("Found Barrier");
+        }else if (shootRaycastAtPlayer(lightLayer).collider != null)
+        {
+            Debug.Log("Found Light");
+        }else if (shootRaycastAtPlayer(playerLayer).collider != null)
+        {
+            Debug.Log("Found Player");
+        }
+    }
+
+    private RaycastHit2D shootRaycastAtPlayer(LayerMask layermask)
+    {
+        return Physics2D.Raycast(this.gameObject.transform.position, player.transform.position, detectionRange,layermask);
+    }
+}
